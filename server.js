4a91e293fd2cee7df35371e24d1d83ea1503aa25
Per Sandstrom <per.sandstrom@purplescout.se>
2014-06-03 21:00:25 +0200

    working ws with fugly express crap

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-var http = require('http');
-var url = require('url');
-var game = require('./game.js');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function (request, response) {
-
-	var url_parts = url.parse(request.url, true);
-
-	var acao = "*";
-	switch (url_parts.pathname) {
-		case '/':
-			response.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': acao});
-			response.end("Hello. Please do stuff");
-			break;
-		case '/play':
-		case '/test':
-			var query = url_parts.query;
-			var data = game.find(query);
-			response.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': acao});
-			response.end(data);
-			break;
-		default:
-			response.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': acao});
-			response.end("Stuff not recognized\n");
-			break;
-	}
+app.get('/', function(req, res){
+	res.sendfile('index.html');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
